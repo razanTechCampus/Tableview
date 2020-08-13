99b6753d7e18227a49011aa2c052398b519feb8e
RazanAlAli <razan.techcampus@gmail.com>
2020-08-13 21:33:51 +0300

    Initial Commit

---
+++ Tableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tableview
+//
+//  Created by TechCampus on 8/13/20.
+//  Copyright © 2020 TechCampus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
